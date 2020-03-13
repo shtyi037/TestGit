9985170039191d6317b129875d6084f90d84b376
bocky <bocky.yu@yjtech.tw>
2020-03-13 17:39:50 +0800

    del all

--- test.py
+++ test.py
@@ -1,28 +1,7 @@
 #coding:utf-8
 import os
 
-def a_apple():
-    print("i am apple")
-    print("i am banana")
 
-def account():
-    print('name')
-    print("account")
-
-def password():
-    print("A~Z")
-
-def game_id():
-    print("game_id")
-
-def prints_number():
-    print("1 2 3 4 ")
-
-def printQA():
-    print("QA 1,")
-
-def printDevDate():
-    print("每周開發的新分支")
 
 if __name__ == "__main__":
-    a_apple()
+    pass
