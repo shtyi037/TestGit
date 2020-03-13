c124d24927c4d7f8dd560222825ef4d6517c93e9
bocky <bocky.yu@yjtech.tw>
2020-03-13 16:00:18 +0800

    add banana

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 #coding:utf-8
+import os
 
-print("who are you?")
-print("iam master")
+def a_apple():
+    print("i am apple")
+
+def a_banana():
+    print("i am banana")
+
+if __name__ == "__main__":
+    a_apple()
