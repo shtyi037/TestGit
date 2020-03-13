7854ed041043b9e968cadff07efb5f27d1ce5f96
bocky <bocky.yu@yjtech.tw>
2020-03-13 17:21:00 +0800

    增加QA和開發分支

--- test.py
+++ test.py
@@ -18,5 +18,11 @@ def game_id():
 def prints_number():
     print("1 2 3 4 ")
 
+def printQA():
+    print("QA 1,")
+
+def printDevDate():
+    print("每周開發的新分支")
+
 if __name__ == "__main__":
     a_apple()
